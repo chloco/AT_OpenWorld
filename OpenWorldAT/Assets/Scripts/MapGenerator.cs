31be9803deb757d053db1611785affe1718d0b8f
Chloe B <chloe5.brown@live.uwe.ac.uk>
2020-02-05 14:55:04 +0000

    texture generator

--- OpenWorldAT/Assets/Scripts/MapGenerator.cs
+++ OpenWorldAT/Assets/Scripts/MapGenerator.cs
@@ -4,21 +4,80 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColourMap}
+    public DrawMode drawMode;
+
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
     public bool autoUpdate;
     public int octaves;
+    
     public int seed;
     public Vector2 offset;
     public float persistance;
+    [Range(0,1)]
     public float lacunarity;
+    public TerrainType[] regions;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity,offset);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapHeight * mapWidth];
+        for(int y = 0; y< mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i ++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if(drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawNoiseMap(noiseMap);
+        }
+        else if(drawMode == DrawMode.ColourMap)
+        {
+
+        }
+        
     }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1 )
+        {
+            mapWidth = 1;
+        }
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if(octaves < 0 )
+        {
+            octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
